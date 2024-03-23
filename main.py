4dd448c11107deae6f8b14be95d0655837bf0bc2
BenjGundersen <121984660+BenjGundersen@users.noreply.github.com>
2024-03-23 00:05:27 -0600

    added more code to answer questions about the data

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pandas as pd
 
 df = pd.read_csv("./dataset/laptops.csv")
+
+# what is the count of the different processor brands?
 print(df["processor_brand"].value_counts())
+print()
 
-print(df[df["processor_brand"] == "amd"])
+# how to see data for laptops of a certain processor brand and price?
+df_cpu = df["processor_brand"] == "amd"
+df_price = df["Price"] < 22000
+print(df[df_cpu & df_price])
+print()
 
+# what if we want to see the a descending or ascending list of prices? (5 laptops)
+df_sorted_price = df.sort_values("Price", ascending = True).head()
+print(df_sorted_price)
